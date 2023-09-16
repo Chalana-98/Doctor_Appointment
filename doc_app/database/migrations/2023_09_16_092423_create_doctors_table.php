e85918c9c753a1d52adf59ef7776431d38eba56d
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-09-16 16:40:24 +0530

    updated
    updated

---
+++ doc_app/database/migrations/2023_09_16_092423_create_doctors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('doc_id')->unique();
+            $table->string('category')->nullable();
+            $table->unsignedBigInteger('patients')->nullable();
+            $table->unsignedBigInteger('experience')->nullable();
+            $table->longText('bio_data')->nullable();
+            $table->string('status')->nullable();
+
+            //this is state that this doc_id refer to id on users table
+
+            $table->foreign('doc_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
